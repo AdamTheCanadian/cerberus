42b377e024d1ac11966b391cb5470af6e9fcbf05
Adam The Canadian <adam.jarrett.clare@gmail.com>
2023-06-23 11:11:32 -0600

    Adding units library

---
+++ core/include/core/units.h
@@ -0,0 +1,22 @@
+#ifndef CERBERUS_TOP_CORE_INCLUDE_CORE_UNITS_H_
+#define CERBERUS_TOP_CORE_INCLUDE_CORE_UNITS_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+  double rad;
+} Radians;
+
+typedef struct {
+  double deg;
+} Degrees;
+
+typedef struct {
+  double m;
+} Metres;
+
+#ifdef __cplusplus
+}
+#endif
+#endif
